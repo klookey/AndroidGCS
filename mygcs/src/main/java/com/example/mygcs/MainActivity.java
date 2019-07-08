1894a8c06afb866cb5676d8308e0434b83f573b1
은지 정 <seyrahan22@gmail.com>
2019-07-08 13:55:04 +0900

    androidGCS mapView로 네이버 지도 연결

--- mygcs/src/main/java/com/example/mygcs/MainActivity.java
+++ mygcs/src/main/java/com/example/mygcs/MainActivity.java
@@ -1,25 +1,75 @@
 package com.example.mygcs;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.UiThread;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
 
-public class MainActivity extends AppCompatActivity {
-
-    MapFragment mNaverMapFragment = null;
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fm = getSupportFragmentManager();
-        mNaverMapFragment = (MapFragment)fm.findFragmentById(R.id.map);
-        if (mNaverMapFragment == null) {
-            mNaverMapFragment = MapFragment.newInstance();
-            fm.beginTransaction().add(R.id.map, mNaverMapFragment).commit();
-        }
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+    }
+
+    @UiThread
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
